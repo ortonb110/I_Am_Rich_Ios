44cfb147b3a5a2ff0b1688e2c5fd7f116199e418
Kobe Dev <ortonb110@gmail.com>
2024-03-03 16:45:36 +0000

    Initial Commit

---
+++ Storyboard_lesson_01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storyboard_lesson_01
+//
+//  Created by Kobe Dev on 03/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
